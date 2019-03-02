6a0c96ce486929ceee39432020500a84979becaa
Virgil John Lomocso <virgil@fullscale.io>
2019-03-02 20:08:44 +0800

    Initial commit

---
+++ MicrophonePermission/MainActivity.cs
@@ -0,0 +1,99 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Genetics.Attributes;
+using Genetics;
+using System;
+using Android.Content.PM;
+using Android.Runtime;
+using Android;
+
+namespace MicrophonePermission
+{
+    [Activity(Label = "MicrophonePermission", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        private const int RecordAudioRequestCode = 0;
+
+        [Splice(Resource.Id.record)]
+        private Button _record;
+        private Recorder _recorder;
+
+        [SpliceClick(Resource.Id.record)]
+        private void RecordClicked(object sender, EventArgs e)
+        {
+            if (IsRecordAudioGranted())
+            {
+                ToggleRecord();
+            }
+            else
+            {
+                RequestPermissions(new string[] { Manifest.Permission.RecordAudio }, RecordAudioRequestCode);
+            }
+        }
+
+        private bool IsRecordAudioGranted()
+        {
+            return CheckSelfPermission(Manifest.Permission.RecordAudio)
+                == Permission.Granted;
+        }
+
+        private void ToggleRecord()
+        {
+            if (_recorder.IsRecording)
+            {
+                _recorder.Stop();
+                _record.Text = Resources.GetString(Resource.String.start_record);
+            }
+            else
+            {
+                _recorder.Start();
+                _record.Text = Resources.GetString(Resource.String.stop_record);
+            }
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            _recorder = new Recorder();
+
+            Geneticist.Splice(this);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            switch (requestCode)
+            {
+                case RecordAudioRequestCode:
+                    if (grantResults.Length > 0
+                        && grantResults[0] == Permission.Granted)
+                    {
+                        ToggleRecord();
+                    }
+                    else
+                    {
+                        var alertBuilder = new AlertDialog.Builder(this);
+                        alertBuilder.SetPositiveButton(Android.Resource.String.Ok,(sender, e) => { });
+                        alertBuilder.SetMessage(Resource.String.please_accept_message);
+                        var dialog = alertBuilder.Create();
+                        dialog.Show();
+                    }
+                    return;
+            }
+
+        }
+
+        protected override void OnDestroy()
+        {
+            Geneticist.Sever(this);
+            _recorder.Dispose();
+            base.OnDestroy();
+        }
+    }
+}
+
---
+++ MicrophonePermission/Recorder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Android.Media;
+
+namespace MicrophonePermission
+{
+    public class Recorder : IDisposable
+    {
+        public const int DefaultBufferSize = 4000;
+        public const ChannelIn Channel = ChannelIn.Mono;
+        public const Encoding AudioEncoding = Encoding.Pcm16bit;
+
+        private static readonly int[] SAMPLE_RATES = { 44100, 22050, 11025, 16000, 8000 };
+        private readonly AudioRecord _recorder;
+        private readonly int _sampleRate;
+        private readonly int _bufferSize;
+        public bool IsRecording { get; private set; }
+
+        public Recorder()
+        {
+            _sampleRate = Init();
+
+            _bufferSize = AudioRecord.GetMinBufferSize(_sampleRate, Channel, AudioEncoding) * 3;
+
+            _recorder = new AudioRecord(AudioSource.Mic,
+                                        _sampleRate, Channel,
+                                        AudioEncoding, _bufferSize);
+        }
+
+        public static int Init()
+        {
+            foreach (var rate in SAMPLE_RATES)
+            {  // add the rates you wish to check against
+                var bufferSize = AudioRecord.GetMinBufferSize(rate, Channel, AudioEncoding);
+                if (bufferSize > 0)
+                {
+                    return rate;
+                }
+            }
+
+            throw new NotSupportedException("Sample rate is not supported.");
+        }
+
+        public void Start()
+        {
+            _recorder.StartRecording();
+            IsRecording = true;
+        }
+
+        public void Stop()
+        {
+            _recorder.Stop();
+            IsRecording = false;
+        }
+
+        public void Dispose()
+        {
+            _recorder.Dispose();
+        }
+    }
+}
